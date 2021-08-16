e78178396da3a171d417281e7ea830385a580895
LiCai <1981824361@qq.com>
2021-08-16 16:23:38 +0800

    模板快捷 配置

---
+++ ViteApp/src/components/Test.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author       : LiCai
+ * @connect      : 1981824361@qq.com
+ * @Date         : 2021-08-16 16:15:15
+ * @LastEditors  : LiCai
+ * @LastEditTime : 2021-08-16 16:16:33
+ * @Description  : vbase 快捷模板
+ * @FilePath     : /VUESTU/ViteApp/src/components/Test.vue
+-->
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- ViteApp/vite.config.js
+++ ViteApp/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
+  alias: {
+    '@': path.resolve(__dirname, 'src') // 别名配置
+  },
   plugins: [vue()]
 })
